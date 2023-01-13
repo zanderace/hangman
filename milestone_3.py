4a23f79f8a8f6cb9a35bd8cf22990b7462a720bf
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-13 23:40:59 +0000

    adding check to see if guess is correct

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,14 @@
+import random 
+
+#list of fruits that guess word is picked from
+word_list = ['apple','pear','grapefruit','banana','kiwi']
+
+# select a random fruit from the list and assign to word
+word = random.choice(word_list)
+
+#print(word)
+
+#check guess is one letter
 while True:
     guess = input("Guess a letter: ")
     if len(guess) == 1 &  guess.isalpha():
@@ -5,3 +16,9 @@ while True:
         
     else:
         print("Invalid letter. Please, enter a single alphabetic letter")
+
+#check if guess is correct
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry , {guess} is not in the word. Try again.")
