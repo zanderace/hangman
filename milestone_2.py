8d1229b95cea0a4a9ab2ac592c70e7611b1ebc55
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-13 23:20:27 +0000

    adding check that guess is one letter

--- milestone_2.py
+++ milestone_2.py
@@ -4,6 +4,11 @@ word_list = ['apple','pear','grapefruit','banana','kiwi']
 #print(word_list)
 
 word = random.choice(word_list)
-print(word)
+#print(word)
 
 guess = input('Enter a single letter: ')
+
+if len(guess) == 1 &  guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! This is not a valid input.")
