a2de667d0b18e8afc1d4be0162f3de681a9e22e6
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-13 23:47:35 +0000

    converting guess check and user input blocks to functions. calling ask for input function

--- milestone_3.py
+++ milestone_3.py
@@ -8,17 +8,23 @@ word = random.choice(word_list)
 
 #print(word)
 
-#check guess is one letter
-while True:
-    guess = input("Guess a letter: ")
-    if len(guess) == 1 &  guess.isalpha():
-        break
-        
+#function to check if guess is correct
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetic letter")
+        print(f"Sorry , {guess} is not in the word. Try again.")
 
-#check if guess is correct
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry , {guess} is not in the word. Try again.")
+#function to check guess is one letter
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter: ")
+        if len(guess) == 1 &  guess.isalpha():
+            break
+            
+        else:
+            print("Invalid letter. Please, enter a single alphabetic letter")
+    check_guess(guess)
+
+ask_for_input()
