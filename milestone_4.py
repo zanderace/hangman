2550004fd0119d13768a6e0a00060135250f461b
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 12:56:56 +0000

    removed definition from self.word_guessed for now

--- milestone_4.py
+++ milestone_4.py
@@ -4,7 +4,7 @@ class Hangman:
     
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
-        self.word_guessed = list(self.word)
+        self.word_guessed
         self.num_letters
         self.num_lives
         self.word_list
