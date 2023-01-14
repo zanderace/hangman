d103816ecb5777a3ec14f77388f07285cfd40340
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 14:45:54 +0000

    print statement editing

--- milestone_4.py
+++ milestone_4.py
@@ -16,17 +16,15 @@ class Hangman:
             print(f"Good guess! {guess} is in the word.")
 
     def ask_for_input(self):
-
         while True:
             guess = input("Please enter a single alphabetic letter: ")
-            if len(guess) != 1 or guess.isalpha()==False:
+            if len(guess) != 1 or not guess.isalpha():
                 print("Invalid letter. Please, enter a single alphabetical character.")
-                
             elif guess in self.list_of_guesses:
-                print("You already tried that letter")
-
+                print("You already tried that letter!")                
             else:
                 self.check_guess(guess)
+                self.list_of_guesses.append(guess)
                 break
                 
 
