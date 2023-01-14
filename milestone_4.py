b984bdad936dbee04a3df2c6d78f5262c9be191b
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 13:25:16 +0000

    setting number of lives to take value of argument passed to __init__

--- milestone_4.py
+++ milestone_4.py
@@ -6,7 +6,7 @@ class Hangman:
         self.word = random.choice(word_list)
         self.word_guessed = list('_'*len(self.word))
         self.num_letters = len(set(self.word))
-        self.num_lives = 0
+        self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
 
