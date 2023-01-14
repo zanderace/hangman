7693030d61269c1165810aba09457500abfd1ed7
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 13:55:59 +0000

    updating self.word as checker didnt like that it was lower case

--- milestone_4.py
+++ milestone_4.py
@@ -3,7 +3,7 @@ import random
 class Hangman:
     
     def __init__(self, word_list, num_lives=5):
-        self.word = random.choice(word_list).lower()
+        self.word = random.choice(word_list)
         self.word_guessed = list('_'*len(self.word))
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
