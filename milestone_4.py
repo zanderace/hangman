36ba6e3d215cb60cc4976640a9a37cda546bcf98
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 13:14:31 +0000

    making self.word_guessed output a list of underscores of same length as guess word

--- milestone_4.py
+++ milestone_4.py
@@ -4,8 +4,10 @@ class Hangman:
     
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
-        self.word_guessed = []
+        self.word_guessed = list('_'*len(self.word))
         self.num_letters = 0
         self.num_lives = 0
         self.word_list = word_list
         self.list_of_guesses = []
+
+print(Hangman(['Apple']).word_guessed)
