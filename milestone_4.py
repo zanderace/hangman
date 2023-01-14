515f4a74b396e9762a701e4fe7f1a4677d651c15
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 13:53:09 +0000

    made guess word lower case for check

--- milestone_4.py
+++ milestone_4.py
@@ -3,7 +3,7 @@ import random
 class Hangman:
     
     def __init__(self, word_list, num_lives=5):
-        self.word = random.choice(word_list)
+        self.word = random.choice(word_list).lower()
         self.word_guessed = list('_'*len(self.word))
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
@@ -12,7 +12,7 @@ class Hangman:
 
     def check_guess(self, guess):
         self.guess = guess.lower()
-        if guess in self.word:
+        if self.guess in self.word:
             print(f"Good guess! {guess} is in the word.")
 
     def ask_for_input(self):
@@ -26,6 +26,6 @@ class Hangman:
 
             else:
                 self.check_guess(guess)
-                break
+                
 
 Hangman(['Apple']).ask_for_input()
