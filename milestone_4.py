c930df92dac5855ed298398e6b7508a93aa0a04c
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 17:25:56 +0000

    Updating check_guess to remove a life if guess is wrong and print out letting user know this

--- milestone_4.py
+++ milestone_4.py
@@ -18,6 +18,10 @@ class Hangman:
                 if letter == guess:
                     self.word_guessed[index] = guess
             self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
     
     def ask_for_input(self):
         while True:
