162ab3adfd8c7c730263b976e2a9bb4947f135c8
Alex Jeffreys <acejeffreys@gmail.com>
2023-01-14 13:19:54 +0000

    changing num_letters to be integer showing number of unique letters in guess word

--- milestone_4.py
+++ milestone_4.py
@@ -5,9 +5,9 @@ class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
         self.word_guessed = list('_'*len(self.word))
-        self.num_letters = 0
+        self.num_letters = len(set(self.word))
         self.num_lives = 0
         self.word_list = word_list
         self.list_of_guesses = []
 
-print(Hangman(['Apple']).word_guessed)
+print(Hangman(['Apple']).num_letters)
